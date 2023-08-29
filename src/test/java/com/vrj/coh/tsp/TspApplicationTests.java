ba9d29ce58d15297e5fb4158442dee49ecb2c5a4
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-08-29 12:08:52 -0600

    Creation of project with dependencies:

---
+++ src/test/java/com/vrj/coh/tsp/TspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrj.coh.tsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
