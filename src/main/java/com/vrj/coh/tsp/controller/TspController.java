f85358e87b6a93b3e587d59714b788ed3dd3f352
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-09-06 18:37:16 -0600

    End-point city

--- src/main/java/com/vrj/coh/tsp/controller/TspController.java
+++ src/main/java/com/vrj/coh/tsp/controller/TspController.java
@@ -1,4 +1,33 @@
 package com.vrj.coh.tsp.controller;
 
+import com.vrj.coh.tsp.model.City;
+import com.vrj.coh.tsp.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("tsp/")
 public class TspController {
+
+    @Autowired
+    CityRepository cityRepository;
+
+    @GetMapping("city/{id}")
+    public ResponseEntity<?> getCity(@PathVariable Long id){
+        System.out.println(id);
+        Optional<City> optionalCity = this.cityRepository.findById(id);
+
+        City city = null;
+
+        if(optionalCity.isPresent())
+            city = optionalCity.get();
+
+        return ResponseEntity.ok(city);
+    }
 }
